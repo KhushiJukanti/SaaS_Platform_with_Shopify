5fc0a9927a4a0cbc255b7093c21db1b24dd03721
IK <khushijukanti33@gmail.com>
2024-09-29 19:37:04 +0530

    add shopify route with middleware auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const UserControllers = require('../controller/user') 
+const auth = require('../middleware/userauth');
 
 
 router.post('/register', UserControllers.Register);
 router.post('/login', UserControllers.Login);
+
+router.get('/orders', auth, UserControllers.getShopifyOrders); // Only authenticated users can access this route
 
 module.exports = router
