22098b6e2fc4afc062357817c1091f224a40fb8a
IK <khushijukanti33@gmail.com>
2024-09-30 17:01:21 +0530

    import components into app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './App.css'
+
+import { AuthProvider } from './components/AuthContext';
+
+import Login from './components/Login';
+import Register from './components/Register';
+import ShopifyNavbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      Hi Khushi
-    </div>
+    <AuthProvider>
+      <Router>
+        <ShopifyNavbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Add other routes here */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
